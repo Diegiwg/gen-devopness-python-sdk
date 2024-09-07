78c0e8ce276910a7b793c96fe20d6630a8ae27bd
Diegiwg <diegiwg@gmail.com>
2024-09-07 16:01:26 -0300

    refactor: restart project

--- test.py
+++
@@ -1,8 +0,0 @@
-from dist.services.virtual_hosts import VirtualHosts
-from dist.services.environments import Environments
-
-virtual_hosts = VirtualHosts()
-virtual_hosts.get_status_virtual_host()
-
-environments = Environments()
-environments.list_environment_applications
